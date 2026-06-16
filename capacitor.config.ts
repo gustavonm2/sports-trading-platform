fa3957f225cd192f1138d0bfb7b1406c4c27b364
gustavonm2 <gustavonm2@gmail.com>
2026-06-16 17:46:23 -0300

    feat: integrate Capacitor for iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.trademoreira.app',
+  appName: 'Trademoreira',
+  webDir: 'dist'
+};
+
+export default config;
